654cd4ca8abaa90f491fcfd9f5ad5e7d142a6885
jkohlbach <jkohlbach@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-07-09 23:42:49 +0000

    Version 1.0 - initial commit
    git-svn-id: https://plugins.svn.wordpress.org/javascript-per-page/trunk@738508 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ javascript_per_page.php
@@ -0,0 +1,299 @@
+<?php
+/*
+* Plugin Name: Javascript Per Page
+*
+* Description: Add custom javascript, IE specific javascript and even iOS specific javascript, to any page or post on your Wordpress website.
+*
+* Author: Josh Kohlbach
+* Author URI: http://www.codemyownroad.com
+* Plugin URI: http://www.codemyownroad.com/products/javascript-per-page-wordpress-plugin/ 
+* Version: 1.0
+*/
+
+
+/*******************************************************************************
+** addCustomJS()
+**
+** Echos a javascript to the header if it exists
+**
+** @since 0.2
+*******************************************************************************/
+function addCustomJS($jsName, $delimiter = '') {
+	if (empty($jsName))
+		return;
+	
+	$possible_src_1 = trailingslashit(get_stylesheet_directory()) . 'js/' . 
+		$delimiter . $jsName . '.js';
+		
+	$possible_src_2 = trailingslashit(get_stylesheet_directory()) . 
+		$delimiter . $jsName . '.js';
+	
+	$use_src_1 = false;	
+	
+	// Retrieve plugin options
+	$javascriptPerPage = get_option('javascriptPerPage');
+	
+	// Optionally, check if the file exists or not
+	if ($javascriptPerPage['check_for_file'] == 'on') {
+		if (file_exists($possible_src_1)) {
+			// file exists in /js directory, give preference to that sheet
+			$use_src_1 = true;
+		} else {
+			// file doesn't exists in /js dir, check the root dir
+			if (!file_exists($possible_src_2)) {
+				// file doesn't exist in the root dir either
+				return;
+			}
+		}
+	} else {
+		// need both files because we're just print it regardless of whether it
+		// exists or not
+		$src1 = trailingslashit(get_bloginfo('stylesheet_directory')) . 'js/' . 
+			$delimiter . $jsName . '.js';
+		$src2 = trailingslashit(get_bloginfo('stylesheet_directory')) . 
+			$delimiter . $jsName . '.js';
+	}
+	
+	if ($use_src_1) {
+		// File exists in /js dir
+		$src = trailingslashit(get_bloginfo('stylesheet_directory')) . 'js/' . 
+			$delimiter . $jsName . '.js';
+	} else {
+		// Use file in root dir
+		$src = trailingslashit(get_bloginfo('stylesheet_directory')) . 
+			$delimiter . $jsName . '.js';
+	}
+	
+	if ($javascriptPerPage['check_for_file'] == 'on') {
+		echo '<script type="text/javascript" src="' . $src . '"></script>';
+	} else {
+		echo '<script type="text/javascript" src="' . $src1 . '"></script>';
+		echo '<script type="text/javascript" src="' . $src2 . '"></script>';
+	}
+		
+}
+
+/*******************************************************************************
+** addCustomJavascripts()
+**
+** Echo a javascript or optionally an array of javascripts back to the browser
+**
+** @since 0.1
+*******************************************************************************/
+function addCustomJavascripts($javascripts) {
+	if (!is_array($javascripts)) {
+		addCustomJS($javascripts, 'page-');
+	} else {
+		foreach ($stylesheets as $jsName) {
+			addCustomJS($jsName,(is_page() ? 'page-' : '')); 
+		}
+	}
+}
+
+/*******************************************************************************
+** addIEJavascripts()
+**
+** Allows you to define ie.js, ie9.js, ie8.js, ie7.js and ie6.js files in your theme 
+** directory (or theme directory plus /js) for isolating IE only Javascript.
+**
+** @since 0.3
+*******************************************************************************/
+function addIEJavascripts() {
+	$ieScripts = array(
+		'IE' => 'ie',
+		'lte IE 8' => 'ie8',
+		'lte IE 7' => 'ie7',
+		'lte IE 6' => 'ie6'
+	);
+	
+	foreach ($ieScripts as $key => $value) {
+		echo '<!--[if ' . $key . ']>';
+		addCustomJS($value);
+		echo "<![endif]-->\n";
+	}
+	
+}
+
+/*******************************************************************************
+** addIosJavascript()
+**
+** Allows you to define a ios.js file in your theme 
+** directory (or theme directory plus /js) for isolating iOS devices (ipad,
+** iphone, ipod).
+**
+** @since 0.5
+*******************************************************************************/
+function addIosJavascript() {
+	if (strpos($_SERVER['HTTP_USER_AGENT'], 'iPad') ||
+		strpos($_SERVER['HTTP_USER_AGENT'], 'iPhone') ||
+		strpos($_SERVER['HTTP_USER_AGENT'], 'iPod')) {
+		addCustomJS('ios');
+	}
+}
+
+/*******************************************************************************
+** javascriptPerPage()
+**
+** Echo a javascript file for the current page
+**
+** @since 0.1
+*******************************************************************************/
+function javascriptPerPage() {
+	global $wp_query;
+	$page_obj = $wp_query->get_queried_object();
+	
+	if (is_page()) {
+		addCustomJavascripts(
+			$page_obj->post_name
+		);
+	} else if (is_author()) {
+		addCustomJavascripts(
+			array(
+				'user', 
+				'user-' . $page_obj->user_login
+			)
+		);
+	} else if (is_singular()) {
+		addCustomJavascripts(
+			array(
+				$page_obj->post_type, 
+				$page_obj->post_type . '-' . $page_obj->post_name
+			)
+		);
+	} else if (is_category()) {
+		addCustomJavascripts(
+			array(
+				'category',
+				'category-' . $page_obj->slug
+			)
+		);
+	} else if (is_tag()) {
+		addCustomJavascripts(
+			array(
+				'tag',
+				'tag-' . $page_obj->slug
+			)
+		);
+	} else if (is_archive()) {
+		addCustomJavascripts(
+			array(
+				'archive'
+			)
+		);
+	} else if (is_home()) {
+		addCustomJavascripts(
+			array(
+				'home'
+			)
+		);
+	} else if (is_front_page()) {
+		addCustomJavascripts(
+			array(
+				'front-page'
+			)
+		);
+	}
+	
+	$javascriptPerPage = get_option('javascriptPerPage');
+	
+	if (!is_admin() && $javascriptPerPage['add_ie_stylesheets'] == 'on'){
+		addIEJavascripts();
+	}
+	
+	if (!is_admin() && $javascriptPerPage['add_ios_stylesheet'] == 'on'){
+		addIosJavascript();
+	}
+}
+
+/*******************************************************************************
+** javascriptPerPageMenu()
+**
+** Setup the plugin options menu
+**
+** @since 0.2
+*******************************************************************************/
+function javascriptPerPageMenu() {
+	if (is_admin()) {
+		register_setting('javascript-per-page', 'javascriptPerPage');
+		add_options_page('Javascript Per Page Settings', 'Javascript Per Page', 'administrator', __FILE__, 'javascriptPerPageOptions', plugins_url('/images/icon.png', __FILE__));
+	}
+}
+
+/*******************************************************************************
+** javascriptPerPageOptions()
+**
+** Present the options page
+**
+** @since 0.2
+*******************************************************************************/
+function javascriptPerPageOptions() {
+	if (!current_user_can('manage_options'))  {
+		wp_die( __('You do not have suffifient permissions to access this page.') );
+	}
+	
+	echo '<div class="wrap">' . screen_icon() . '<h2>Javascript Per Page</h2>';
+	
+	$javascriptPerPage = get_option('javascriptPerPage');
+	$javascriptPerPage['check_for_file'] = $javascriptPerPage['check_for_file'] ? 'checked="checked"' : '';
+	$javascriptPerPage['add_ie_javascripts'] = $javascriptPerPage['add_ie_javascripts'] ? 'checked="checked"' : '';
+	$javascriptPerPage['add_ios_javascript'] = $javascriptPerPage['add_ios_javascript'] ? 'checked="checked"' : '';
+	
+	echo '<form method="post" action="options.php">';
+	
+	wp_nonce_field('update-options');
+	settings_fields( 'javascript-per-page' );
+	
+	echo '<table class="form-table">
+	<tr valign="top">
+	<th scope="row" style="white-space: nowrap;">Check for files before placing in header?</th>
+	<td>
+	<input type="checkbox" name="javascriptPerPage[check_for_file]" id="check_for_file" ' . 
+	$javascriptPerPage['check_for_file'] . ' />
+	</td></tr>
+	
+	<tr valign="top">
+	<th scope="row" style="white-space: nowrap;"><label for="javascriptPerPage[add_ie_javascripts]">Add IE specific javascript files?</label></th>
+	<td>
+	<input type="checkbox" name="javascriptPerPage[add_ie_javascripts]" id="add_ie_javascripts" ' . 
+	$javascriptPerPage['add_ie_javascripts'] . ' />
+	<p><span class="description">Allows you to define ie.js, ie9.js, ie8.js, ie7.js and ie6.js files in your theme directory (or a /js subdirectory in your theme)<br />
+	for isolating IE only Javascript.</span></p>
+	</td></tr>
+	
+	<tr valign="top">
+	<th scope="row" style="white-space: nowrap;"><label for="javascriptPerPage[add_ios_javascript]">Add iOS javascript?</label></th>
+	<td>
+	<input type="checkbox" name="javascriptPerPage[add_ios_javascript]" id="add_ios_javascript" ' . 
+	$javascriptPerPage['add_ios_javascript'] . ' />
+	<p><span class="description">Allows you to define ios.js in your theme directory to add a specific Javascript for iOS devices (ipad, iphone, ipod)</span></p>
+	</td></tr>
+	
+	</table>
+	
+	<input type="hidden" name="page_options" value="javascriptPerPage" />
+		
+	<p class="submit">
+	<input type="submit" class="button-primary" value="Save Changes" />
+	</p>
+	
+	</form>
+	</div>';
+
+}
+
+
+/*******************************************************************************
+** initjavascriptPerPage()
+**
+** Initialize the stylesheet per page plugin
+**
+** @since 0.1
+*******************************************************************************/
+function initJavascriptPerPage() {
+	add_filter('wp_head', 'javascriptPerPage', 999);
+	add_action('admin_menu', 'javascriptPerPageMenu');	
+}
+
+add_action('init', 'initJavascriptPerPage', 1);
+
+?>
---
+++ readme.txt
@@ -0,0 +1,75 @@
+=== Javascript Per Page ===
+Contributors: jkohlbach
+Donate link: http://www.codemyownroad.com
+Tags: javascript, javascript per page, ie, ie javascript, ios javascript, ios, post javascript, page javascript, category javascript, tag javascript
+Requires at least: 3.0
+Tested up to: 3.2
+Stable tag: trunk
+
+Add custom javascript, IE specific javascript and even iOS specific javascript, to any page or post on your Wordpress website.
+
+== Description ==
+
+Javascript Per Page adds a custom javascript file to all of the pages on your WordPress install with options to check for the existence of the javascript file before adding.
+
+Allows for adding IE specific javascript all the way back to version 6. Just enable in the plugin options and add the javascript files to your theme directory.
+
+Javascript Per Page also allows adding of an iOS specific javascript file. Simply enable in the plugin options and add the javascript to your theme directory.
+
+This plugin supports use with custom post types, category, and tag pages.
+
+== Installation ==
+
+1. Upload the plugin to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+
+To use this plugin, just add javascript files in the following formats to your theme's base directory or a "js" subdirectory inside your theme's base directory:
+
+* For pages create a file called page-[page_name].js where "[page_name]" is replaced with the slug of the page. Eg: page-about.js.
+
+* For author pages create a file called user.js and for individual user pages user-[username].js where "[username]" is replaced with the user's login name.
+
+* For different post types user [post_type].js where "[post_type]" is replaced with the name of the custom post type. You can also use just 'post' for regular posts. Eg. post.js.
+
+* For individual posts use [post_type]-[post_name].js where "[post_type]" is replaced with the name of the custom post type or just 'post' for regular posts, and "[post_name]" is the slug of the post. Eg. post-10-tips-for-baking-pies.js, or, say if you had a "recipes" post type use something like recipes-classic-french-cheesecake.js.
+
+* For tag pages use tag.js or tag-[tag_name].js where "[tag_name]" is replaced with the slug of the tag
+
+* For category pages use category.js or category-[category_name].js where "[category_name]" is replaced with the slug of the category
+
+* For archive pages use archive.js
+
+* For home and front pages use home.js and front-page.js respectively
+
+* To activate IE specific javascript files just create js files with the following naming: ie.js (covers all IE versions), ie8.js (covers IE 8 and below), ie7.js (covers IE 7 and below), ie6.js (covers IE 6 and below).
+
+* To activate a iOS specific javascript create ios.js and enable in the plugin options.
+
+NOTE: You can place your custom JS files in either the base directory of your theme, or in a "js" subdirectory inside your theme, but try to be consistent with where you place them.
+
+== Frequently Asked Questions ==
+
+= How do I activate IE javascript files? =
+To activate javascript files enable the option in the plugin options and create js files with the following naming convention:
+ie.js (overrides for all IE versions), ie8.js (overrides for IE 8 and below), ie7.js (overrides for IE 7 and below), ie6.js (overrides for IE 6 and below).
+
+= How do I get Javascript Per Page to check for the files first before trying to include them? =
+
+Easy, just tick the option under the Javascript Per Page admin options under found under the Settings menu.
+
+= How do I enable an iOS javascript? =
+
+To activate an iOS specific javascript create ios.js and place it in your theme's base directory or js subdirectory inside your theme's base directory. You will also need to enable the checkbox in the plugin options.
+
+== Screenshots ==
+
+N/A
+
+== Upgrade Notice ==
+
+N/A
+
+== Changelog ==
+
+* 1.0 Initial version
+ - Code base copied from [Stylesheet Per Page](http://wordpress.org/plugins/stylesheet-per-page/) plugin and adjusted for Javascript
